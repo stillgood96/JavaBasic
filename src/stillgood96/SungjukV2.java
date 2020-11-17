3e6ca878018da6e31bfdc3e92ab2f9ed99b9c1e5
stillgood96 <a787574@naver.com>
2020-11-17 12:58:12 +0900

    성적프로그램v2  선생님버전

--- src/stillgood96/SungjukV2.java
+++ src/stillgood96/SungjukV2.java
@@ -20,11 +20,11 @@ import java.util.Scanner;
 public class SungjukV2 {
     public static void main(String[] args) {
         //변수선언  +선언할때 초기화도 해두면 좋다. +줄복사 ctrl+d
-        Scanner scanner=new Scanner(System.in);
+
         String name = "선우";
-        int kor=scanner.nextInt();
-        int eng=scanner.nextInt();;
-        int mat=scanner.nextInt();;
+        int kor=99;
+        int eng=0;
+        int mat=0;
         int sum = 0;
         double mean  = 0.0;
         char grd = '가';
@@ -36,38 +36,33 @@ public class SungjukV2 {
 
 
         //처리
-        sum = kor + eng + mat;
-                                 //mean= (kor + eng + mat);
-        mean = (double)sum/3;
+        //성적 데이터를 키보드로 입력받기 위해
+        //Scanner 클래스 초기화
+        Scanner sc=new Scanner(System.in);
+        System.out.print("이름을 입력하세요 :");          //이름을 입력하세요 : 줄바꿈이 안일어나고 입력받을 수 있게 print문으로 한다.
+        name=sc.nextLine();  //문자를 받을때는 nextLine();
+        System.out.print("국어점수를 입력하세요 :");
+        kor=sc.nextInt();    //정수를 받을때는 nextInt(); 실수는 Double
+        System.out.print("영어점수를 입력하세요 :");
+        eng=sc.nextInt();
+        System.out.print("수학점수를 입력하세요 :");
+        mat=sc.nextInt();
 
-        //스위치문
-        switch((int)(mean/10)){
-            case 10 : grd='수';
-            break;
-            case 9 : grd='수';
-            break;
-            case 8 : grd='우';
-            break;
-            case 7 : grd='미';
-            break;
-            case 6 : grd='양';
-            break;
-            case 5 : grd='가';
-            break;
-            case 4 : grd='가';
-            break;
-            case 3 : grd='가';
-            break;
-            case 2 : grd='가';
-            break;
-            case 1 : grd='가';
-            break;
-            case 0 : grd='가';
-            break;
+        sum = kor+mat+eng;   //총합
+        mean = (double)sum/3; //평균
 
+        //학점계산은 switch문으로 처리
+        switch((int)(mean/10)) {
+            case 10:
+            case 9: grd='수'; break;
+            case 8: grd='우'; break;
+            case 7: grd='미'; break;
+            case 6: grd='양'; break;
+            default: grd='가'; break;
         }
-
-
+        //String.format(형식지정자, 변수들)
+        mean = Double.parseDouble(
+                String.format("%.1f",mean));
 
 
 
@@ -80,6 +75,7 @@ public class SungjukV2 {
         System.out.println("-----------");
         System.out.println("총점:"+sum);
         System.out.println("평균:"+mean);
+//        System.out.printf("평균:%.1f\n",mean);        //alt+shift+방향키 누르면 줄바꿈 비추천
         System.out.println("학점:"+grd);
 
 
