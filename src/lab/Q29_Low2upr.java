74e6e1e77c3dc7064a27b17eefb276cf68b10308
stillgood96 <a787574@naver.com>
2020-11-18 17:09:00 +0900

    Q29_low2upr 메서드x 버전

---
+++ src/lab/Q29_Low2upr.java
@@ -0,0 +1,20 @@
+package lab;
+
+import java.io.IOException;
+
+public class Q29_Low2upr {
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            System.out.print("알파벳 소문자입력시 대문자로 변형됩니다(0입력시 종료) :");
+            int num = System.in.read(); //아스키코드로 입력하는 것 같다.
+            System.in.read();
+            if (num < 97 || num > 122) {
+                if (num==48) {break;}  //아스키코드에선 0= 48
+                System.out.print("소문자만 입력하세요 다시 입력해주세요:");
+                num = System.in.read();
+            } else {
+                System.out.println((char) (num - 32));
+            }
+        }
+    }
+}
